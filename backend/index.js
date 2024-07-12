8184f7171ac8f617900fc51d5d72e4e6451a2866
shreshth800 <shreshthsharma800@gmail.com>
2024-07-12 23:07:32 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 dotenv.config();
 
